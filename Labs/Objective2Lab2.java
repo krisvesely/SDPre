cf5316a8f59e6b411653cbb81fa5cdde3d9412da
krisvesely <kristine.vesely@gmail.com>
2023-02-16 16:28:18 -0500

    complete Objective2Lab2

---
+++ Labs/Objective2Lab2.java
@@ -0,0 +1,8 @@
+public class Objective2Lab2 {
+	public static void main(String[] args) {
+		String name = "Kris Vesely";
+		int age = 52;
+		String state = "Maryland";
+		System.out.println("Name: " + name + " Age: " + age + " State: " + state);
+	}
+}
