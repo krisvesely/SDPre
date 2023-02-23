03ca11bafdc27d45018108fc15c399070e0dc0bf
krisvesely <kristine.vesely@gmail.com>
2023-02-23 12:49:16 -0500

    complete Objective8Lab2

---
+++ Labs/Objective8Lab2.java
@@ -0,0 +1,10 @@
+public class Objective8Lab2 {
+	public static void main(String[] args) {
+		int sum = 0, i = 0;
+
+		for (i = 1; i <=20; i++) {
+			sum = sum + i;
+		}
+		System.out.println(sum);
+	}
+}
